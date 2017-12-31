e906c440aa70f0eab91af6b99c248aed1d864176
nazaif <nazaifmoid@gmail.com>
2017-12-31 15:08:42 +0530

    Stage 1 Resubmission

--- app/src/main/java/com/example/android/popularmoviesapp/MovieAPIinterface.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieAPIinterface.java
@@ -8,7 +8,6 @@ import retrofit2.http.Path;
  * Created by nazaif on 21/12/17.
  */
 
-
 public interface MovieAPIinterface {
     @GET("movie/{sortby}")
     Call<MovieResults> getPopular(@Path("sortby") String sortBy);
